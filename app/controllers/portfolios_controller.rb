a84ffa3757ef04487d936ab092b598d5263594c7
Siddharth Ahuja <ahujasid@gmail.com>
2018-05-01 22:27:03 +0530

    new and create for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,21 @@ class PortfoliosController < ApplicationController
 		@p_items = Portfolio.all
 	end
 
+	def new
+		@p_item = Portfolio.new
+	end
+
+	def create
+		@p_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @p_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
 end
