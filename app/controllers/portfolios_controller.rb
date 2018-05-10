1a5ae620a9d9367edfa10a34599c5ec27d498626
Siddharth Ahuja <ahujasid@gmail.com>
2018-05-10 22:28:54 +0530

    added delete functionality to portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,6 +41,14 @@ class PortfoliosController < ApplicationController
     @p_item = Portfolio.find(params[:id])
   end
 
+def destroy
+  @p_item = Portfolio.find(params[:id])
 
+   @p_item.destroy
+   
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Blog was successfully destroyed.' }
+    end
+end
 
 end
